f4e1ca0cb9f8144eb400b01b69fcb1430b6a25f0
SNijanthan <nijanthan378@gmail.com>
2025-04-08 17:17:17 +0530

    logout API

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const { validateSignUpData } = require("../utils/inputValidation");
 const { User } = require("../model/User.model");
+const { auth } = require("../middleware/auth.middleware");
 
 require("dotenv").config();
 
@@ -86,6 +87,19 @@ authRouter.post("/login", async (req, res) => {
           user,
         });
     }
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
+
+// User logout
+
+authRouter.post("/logout", auth, async (req, res) => {
+  try {
+    res
+      .cookie("TOKEN", null, { expires: new Date(0), httpOnly: true })
+      .status(200)
+      .json({ success: true, message: "Logged out succesfully..!" });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
