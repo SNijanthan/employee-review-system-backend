9d75ba3431941f014724d976f7497b15e0d69735
SNijanthan <nijanthan378@gmail.com>
2025-04-09 17:37:23 +0530

    cors config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookie_parser = require("cookie-parser");
+const cors = require("cors");
 
 const { connectToDatabase } = require("./config/database");
 const { authRouter } = require("./routes/auth.route");
@@ -12,6 +13,8 @@ const port = 7777;
 
 app.use(express.json());
 app.use(cookie_parser());
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Whitelisting domain name
 
 app.use("/", authRouter);
 app.use("/", userRouter);
